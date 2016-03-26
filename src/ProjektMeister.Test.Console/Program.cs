ff5ba7a06a3b113f1b41448aff2aa810dfc6ef96
Martin Brenn <brenn@depon.net>
2016-03-26 21:58:16 +0100

    Fixed bug and multiple projects are now supported

--- src/ProjektMeister.Test.Console/Program.cs
+++ src/ProjektMeister.Test.Console/Program.cs
@@ -15,17 +15,37 @@ namespace ProjektMeister.Test.Console
             var project = new Project();
             project.StartOfProject.Defined = new DateTime(2016,1,1,12,0,0);
 
-            var activity = new Activity
+            var homeActivity = new Activity
             {
                 Id = 1,
                 Name = "Hausaufgaben",
                 Duration = {Defined = TimeSpan.FromDays(10)}
             };
-            project.Add(activity);
+            project.Add(homeActivity);
+
+            var prepareActivity = new Activity
+            {
+                Id = 2,
+                Name = "Vorbereiten",
+                Duration = { Defined = TimeSpan.FromDays(2) }
+            };
+            homeActivity.Dependencies.Add(prepareActivity);
+            project.Add(prepareActivity);
+
+            var finalizeActivity = new Activity
+            {
+                Id = 3,
+                Name = "Abschließen",
+                Duration = { Defined = TimeSpan.FromDays(1) }
+            };
+            finalizeActivity.Dependencies.AddRange(new [] {homeActivity, prepareActivity});
+            project.Add(finalizeActivity);
 
             var logic = new ProjectTimingCalculator(new ProjectTimingSettings());
             logic.PlanProject(project);
-            System.Console.WriteLine($"{activity}");
+            System.Console.WriteLine($"{prepareActivity}");
+            System.Console.WriteLine($"{homeActivity}");
+            System.Console.WriteLine($"{finalizeActivity}");
 
             System.Console.ReadKey();
         }
